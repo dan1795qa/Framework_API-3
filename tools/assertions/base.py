fec4515a41fbb8a26f96a4fc37f09052c57a5768
Daniil <dan1795dan@gmail.com>
2026-04-23 23:16:24 +0300

    Added base's tool 'assert_equal', 'assert_is_true', 'assert_is_false', 'assert_is_zero'

--- tools/assertions/base.py
+++ tools/assertions/base.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 def assert_status_code(actual: int, expected: int):
     """
     Проверяет, что фактический статус-код ответа соответствует ожидаемому.
@@ -8,7 +11,67 @@ def assert_status_code(actual: int, expected: int):
     """
 
     assert actual == expected, (
-        "Некорректный статус-код." 
-        f"Ожидается статус-код: {expected}." 
+        "Некорректный статус-код."
+        f"Ожидается статус-код: {expected}."
         f"Актуальный стаутс-код: {actual}."
+    )
+
+
+def assert_equal(actual: Any, expected: Any, name: str):
+    """
+    Проверяет, что фактическое значение равно ожидаемому.
+
+    :param name: Название проверяемого значения.
+    :param actual: Фактическое значение.
+    :param expected: Ожидаемое значение.
+    :raises AssertionError: Если фактическое значение не равно ожидаемому.
+    """
+
+    assert actual == expected, (
+        f"Некорректное значение {name}."
+        f"Ожидается статус-код: {expected}."
+        f"Актуальный стаутс-код: {actual}."
+    )
+
+
+def assert_is_true(actual: Any, name: str):
+    """
+    Проверяет, что фактическое значение является истинным.
+
+    :param name: Название проверяемого значения.
+    :param actual: Фактическое значение.
+    :raises AssertionError: Если фактическое значение ложно.
+    """
+
+    assert actual or actual is None, (
+        f'Incorrect value: "{name}". '
+        f'Expected true value but got: {actual}'
+    )
+
+
+def assert_is_false(actual: Any, name: str):
+    """
+    Проверяет, что фактическое значение является ложным.
+
+    :param name: Название проверяемого значения.
+    :param actual: Фактическое значение.
+    :raises AssertionError: Если фактическое значение истинно.
+    """
+    assert not actual, (
+        f'Incorrect value: "{name}". '
+        f'Expected false value but got: {actual}'
+    )
+
+
+def assert_is_zero(actual: Any, name: str):
+    """
+    Проверяет, что фактическое значение равно нулю.
+
+    :param name: Название проверяемого значения.
+    :param actual: Фактическое значение.
+    :raises AssertionError: Если фактическое значение не равно 0.
+    """
+    assert actual == 0, (
+        f'Incorrect value: "{name}". '
+        f'Expected 0 but got: {actual}'
     )
