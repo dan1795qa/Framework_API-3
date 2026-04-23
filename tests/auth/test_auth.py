15a25e77402bae10e2b4d73277481c1ce9547483
Daniil <dan1795dan@gmail.com>
2026-04-23 23:19:20 +0300

    Modified test 'test_register' - added 'assert_register_response' and 'validate_json_schema'

--- tests/auth/test_auth.py
+++ tests/auth/test_auth.py
@@ -1,8 +1,10 @@
 from http import HTTPStatus
 
 from clients.auth.auth_client import AuthClient
-from clients.auth.auth_schema import RegisterRequestSchema
+from clients.auth.auth_schema import RegisterRequestSchema, RegisterResponseSchema
+from tools.assertions.auth import assert_register_response
 from tools.assertions.base import assert_status_code
+from tools.assertions.schema import validate_json_schema
 
 
 class TestAuth:
@@ -10,5 +12,9 @@ class TestAuth:
     def test_register(self, auth_client: AuthClient):
         request = RegisterRequestSchema()
         response = auth_client.register_api(request)
+        response_model = RegisterResponseSchema.model_validate_json(response.text)
 
         assert_status_code(response.status_code, HTTPStatus.CREATED)
+        assert_register_response(response_model)
+
+        validate_json_schema(response.json(), response_model.model_json_schema())
