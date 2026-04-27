7759005ae23706f01c0d634af372b36772128bb1
Daniil <dan1795dan@gmail.com>
2026-04-27 21:55:28 +0300

    Added fixture 'function_register_user'

--- fixtures/auth.py
+++ fixtures/auth.py
@@ -1,8 +1,23 @@
 import pytest
-
+from pydantic import BaseModel
 from clients.auth.auth_client import AuthClient, get_auth_client
+from clients.auth.auth_schema import RegisterRequestSchema, RegisterResponseSchema
+
+
+class AuthFixture(BaseModel):
+    request: RegisterRequestSchema
+    response: RegisterResponseSchema
 
 
 @pytest.fixture
 def auth_client() -> AuthClient:
     return get_auth_client()
+
+
+@pytest.fixture()
+def function_register_user(auth_client: AuthClient) -> AuthFixture:
+    request = RegisterRequestSchema()
+    response = auth_client.register_api(request)
+    response_model = RegisterResponseSchema.model_validate_json(response.text)
+
+    return AuthFixture(request=request, response=response_model)
