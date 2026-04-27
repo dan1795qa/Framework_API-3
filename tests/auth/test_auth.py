6835fd31eeaf5d817284906cded0f734dd59c23c
Daniil <dan1795dan@gmail.com>
2026-04-27 21:52:56 +0300

    Added test 'test_login'

--- tests/auth/test_auth.py
+++ tests/auth/test_auth.py
@@ -1,8 +1,9 @@
 from http import HTTPStatus
 
 from clients.auth.auth_client import AuthClient
-from clients.auth.auth_schema import RegisterRequestSchema, RegisterResponseSchema
-from tools.assertions.auth import assert_register_response
+from clients.auth.auth_schema import RegisterRequestSchema, RegisterResponseSchema, LoginRequestSchema, TokenSchema
+from fixtures.auth import AuthFixture
+from tools.assertions.auth import assert_register_response, assert_token_response
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 
@@ -16,5 +17,18 @@ class TestAuth:
 
         assert_status_code(response.status_code, HTTPStatus.CREATED)
         assert_register_response(response_model)
+
+        validate_json_schema(response.json(), response_model.model_json_schema())
+
+    def test_login(self, auth_client: AuthClient, function_register_user: AuthFixture):
+        request = LoginRequestSchema(
+            email=function_register_user.response.email,
+            password=function_register_user.request.password
+        )
+        response = auth_client.login_api(request)
+        response_model = TokenSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_token_response(response_model)
 
         validate_json_schema(response.json(), response_model.model_json_schema())
