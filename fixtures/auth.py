64a018d1c9c8a5bdb6adcf7871dbba2f62b80503
Daniil <dan1795dan@gmail.com>
2026-05-04 22:09:59 +0300

    Added fixtures 'function_login_user'

--- fixtures/auth.py
+++ fixtures/auth.py
@@ -1,12 +1,17 @@
 import pytest
 from pydantic import BaseModel
 from clients.auth.auth_client import AuthClient, get_auth_client
-from clients.auth.auth_schema import RegisterRequestSchema, RegisterResponseSchema
+from clients.auth.auth_schema import RegisterRequestSchema, RegisterResponseSchema, LoginRequestSchema, TokenSchema
 
 
-class AuthFixture(BaseModel):
+class RegisterFixture(BaseModel):
     request: RegisterRequestSchema
     response: RegisterResponseSchema
+
+
+class LoginFixture(BaseModel):
+    request: LoginRequestSchema
+    response: TokenSchema
 
 
 @pytest.fixture
@@ -15,9 +20,23 @@ def auth_client() -> AuthClient:
 
 
 @pytest.fixture()
-def function_register_user(auth_client: AuthClient) -> AuthFixture:
+def function_register_user(auth_client: AuthClient) -> RegisterFixture:
     request = RegisterRequestSchema()
     response = auth_client.register_api(request)
+    print(response.text)
     response_model = RegisterResponseSchema.model_validate_json(response.text)
 
-    return AuthFixture(request=request, response=response_model)
+    return RegisterFixture(request=request, response=response_model)
+
+
+@pytest.fixture()
+def function_login_user(auth_client: AuthClient, function_register_user: RegisterFixture) -> LoginFixture:
+    request = LoginRequestSchema(
+        email=function_register_user.response.email,
+        password=function_register_user.request.password
+    )
+    response = auth_client.login_api(request)
+    print(response.text)
+    response_model = TokenSchema.model_validate_json(response.text)
+
+    return LoginFixture(request=request, response=response_model)
