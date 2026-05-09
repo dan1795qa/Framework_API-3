11ccae021cd52f49d487ddf06dbbc33ba307de81
Daniil <dan1795dan@gmail.com>
2026-05-09 15:49:37 +0300

    changed 'private_http_builder': set token from fixtures 'private_auth_client'

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -1,33 +1,36 @@
 from httpx import Client
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
-from clients.auth.auth_client import get_auth_client
+from clients.auth.public_auth_client import get_auth_client
 from clients.auth.auth_schema import LoginRequestSchema
 from config import settings
 
 
-class AuthenticationUserSchema(BaseModel, frozen=True):
-    email: str
-    password: str
+# class AuthenticationUserSchema(BaseModel, frozen=True):
+#     email = str
+#     password = str
+#     # access_token: str
+class AuthTokenSchema(BaseModel, frozen=True):
+    access_token: str
 
 
-def get_private_http_client(user: AuthenticationUserSchema) -> Client:
+def get_private_http_client(token: AuthTokenSchema) -> Client:
     """
     Функция создаёт экземпляр httpx.Client с аутентификацией пользователя.
 
-    :param user: Объект AuthenticationUserSchema с email и паролем пользователя.
+    :param token: Объект AuthTokenSchema с токеном пользователя.
     :return: Готовый к использованию объект httpx.Client с установленным заголовком Authorization.
     """
-    # Инициализируем AuthenticationClient для аутентификации
-    authentication_client = get_auth_client()
-
-    # Инициализируем запрос на аутентификацию
-    login_request = LoginRequestSchema(email=user.email, password=user.password)
-    login_response = authentication_client.login(login_request)
+    # # Инициализируем AuthenticationClient для аутентификации
+    # authentication_client = get_auth_client()
+    #
+    # # Инициализируем запрос на аутентификацию
+    # login_request = LoginRequestSchema(email=user.email, password=user.password)
+    # login_response = authentication_client.login(login_request)
 
     return Client(
         timeout=settings.http_client.timeout,
         base_url=settings.http_client.client_url,
         # Добавляем заголовок авторизации
-        headers={"Authorization": f"Bearer {login_response.access_token}"}
+        headers={"Authorization": f"Bearer {token.access_token}"}
     )
