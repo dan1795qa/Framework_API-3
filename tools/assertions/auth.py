e98b9afb23ed9a641b39364d29c4c43ea87d314f
Daniil <dan1795dan@gmail.com>
2026-05-10 12:26:51 +0300

    Added assert 'assert_get_me_response'

--- tools/assertions/auth.py
+++ tools/assertions/auth.py
@@ -1,4 +1,4 @@
-from clients.auth.auth_schema import RegisterResponseSchema, TokenSchema
+from clients.auth.auth_schema import RegisterResponseSchema, TokenSchema, GetMeResponseSchema
 from tools.assertions.base import assert_equal, assert_is_true, assert_is_false, assert_is_zero, assert_is_empty
 
 
@@ -49,3 +49,30 @@ def assert_empty_response(response):
     :raises AssertionError: Если какое-либо из условий не выполняется.
     """
     assert_is_empty(response)
+
+
+def assert_get_me_response(response: GetMeResponseSchema):
+    """
+    Проверяет корректность ответа при запросе авторизированного пользователя о себе.
+
+    :param response: Объект ответа с данными пользователями.
+    :raises AssertionError: Если какое-либо из условий не выполняется.
+    """
+    assert_is_true(response.id, "id")
+    assert_is_true(response.email, "email")
+    assert_is_true(response.username, "username")
+    assert_is_true(response.display_name, "display_name")
+    assert_is_true(response.bio, "bio")
+    assert_is_true(response.avatar_url, "avatar_url")
+    assert_is_true(response.cover_url, "cover_url")
+    assert_is_true(response.role, "role")
+    assert_is_true(response.is_active, "is_active")
+    assert_is_false(response.is_verified, "is_verified")
+    assert_is_false(response.is_private, "is_private")
+    assert_is_true(response.created_at, "created_at")
+    assert_is_true(response.updated_at, "updated_at")
+    assert_is_zero(response.followers_count, "followers_count")
+    assert_is_zero(response.following_count, "following_count")
+    assert_is_zero(response.posts_count, "posts_count")
+    assert_is_false(response.is_following, "is_following")
+    assert_is_false(response.is_followed_by, "is_followed_by")
